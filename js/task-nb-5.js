b24c34d45eb26157928d538d49cd9d152815da9b
Nazar612 <nazarstovba300@gmail.com>
2026-05-31 17:07:02 +0200

    goit-js-hw-05

--- js/task-nb-5.js
+++ js/task-nb-5.js
@@ -5,63 +5,66 @@
 
 
 class Car {
- /*
-  * Додай статичний метод `getSpecs(car)`,
-  * який приймає об'єкт-машину як параметр і виводить
-  * в консоль значення властивостей maxSpeed, speed, isOn, distance и price.
-  */
+   getSpecs(car) {
+   console.log(`MAXspeed: ${car.maxSpeed}, Speed: ${car.speed}, isOn?: ${car.isOn}, Distance: ${car.distance}, Price: ${car.price}`);
+ };
 
- /*
-  * Конструктор отримує об'єкт налаштувань.
-  *
-  * Додай властивості майбутнього екземпляра класу:
-  * speed - поточна швидкість, початкова 0
-  * price - ціна автомобіля
-  * maxSpeed - максимальна швидкість
-  * isOn - заведений автомобіль, значення true або false. Спочатку false
-  * distance - загальний кілометраж, спочатку 0
-  */
- constructor(maxSpeed = 0, price = 0) {
+ constructor(maxSpeed = 0, price = 0, speed = 0 , isOn = false, distance = 0,) {
     this.maxSpeed = maxSpeed;
     this.price = price;
- }
+    this.speed = speed;
+    this.isOn = isOn;
+    this.distance = distance;
+ };
 
- /*
-  * Додай геттер і сеттер для властивості price,
-  * який буде працювати з властивістю ціни автомобіля.
-  */
 
- /*
-  * Додай код для того, щоб завести автомобіль
-  * Записує у властивість isOn значення true
-  */
- turnOn() {}
+ set price(many) {
+   this._price = many
+ };
+  get price() {
+   return this._price
+ };
 
- /*
-  * Додай код для того, щоб заглушити автомобіль
-  * Записує у властивість isOn значення false,
-  * і скидає поточну швидкість в 0
-  */
- turnOff() {}
+ turnOn() {
+   return this.isOn = true;
+ };
+ turnOff() {
+   this.isOn = false;
+   if(this.isOn === false) {
+      this.speed = 0;
+   }
+   return this.isOn
+ };
 
  /*
   * Додає до властивості speed отримане значення,
   * за умови, що результуюча швидкість
   * не більше, ніж значення властивості maxSpeed
   */
- accelerate(value) {}
-
- /*
-  * Забирає від властивості speed отримане значення,
-  * за умови, що результуюча швидкість не менше нуля
-  */
- decelerate(value) {}
+ accelerate(value) {
+   let result = this.speed + value;
+   if(result > this.maxSpeed) {
+      result = 0;
+   }
+   this.speed = result
+ }
+ decelerate(value) {
+   let result = this.speed -= value
+   if(result < 0 || this.isOn === false) {
+      result = 0;
+   }
+   this.speed = result
+ }
 
  /*
   * Додає в поле distance кілометраж (hours * speed),
   * але тільки в тому випадку, якщо машина заведена!
   */
- drive(hours) {}
+ drive(hours) {
+   if(this.isOn) {
+      this.distance = this.speed * hours;
+   }
+ }
 }
 
 const mustang = new Car( 200, 2000);
@@ -70,14 +73,14 @@ mustang.turnOn();
 mustang.accelerate(50);
 mustang.drive(2);
 
-Car.getSpecs(mustang);
+mustang.getSpecs(mustang);
 // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
 
 mustang.decelerate(20);
 mustang.drive(1);
 mustang.turnOff();
 
-Car.getSpecs(mustang);
+mustang.getSpecs(mustang);
 // maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
 
 console.log(mustang.price); // 2000
